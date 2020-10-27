c89cea59e7c0d75a94b64986589c9656514da8b5
alexkimeu0 <kim3ual3x@gmail.com>
2020-10-27 14:46:59 +0300

    Added header slider

---
+++ js/carousel/slider.js
@@ -0,0 +1,20 @@
+$(".slider").owlCarousel({
+	loop: true,
+	margin: 10,
+	responsiveClass: true,
+	autoplay: 15000,
+	responsive: {
+		0: {
+			items: 1,
+			nav: false,
+		},
+		600: {
+			items: 1,
+			nav: false,
+		},
+		1000: {
+			items: 1,
+			nav: false,
+		},
+	},
+});
