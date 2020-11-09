398b61a0271973e77d896ab21ccb21ac968b425a
alexkimeu0 <kim3ual3x@gmail.com>
2020-11-09 14:35:40 +0300

    Fixed issues with mobile navigation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-let sidebar = document.getElementById('menu');
-sidebar.classList.remove('menu')
-sidebar.classList.add('sidebar')
+$(window).on('resize', () => {
+    if ($(window).width() < 600) {
+        $('#menu').removeClass('menu');
+        $('#menu').addClass('sidebar');
+    } else {
+        $('#menu').removeClass('sidebar');
+        $('#menu').addClass('menu');        
+    }
+});
